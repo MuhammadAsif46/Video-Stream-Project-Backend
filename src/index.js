03ddbb85246b9b3f5219c0ed9c5092ca8b91c04a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-11 16:42:06 +0500

    add utilities and middlewares usage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
 // SECOND APPROACH -- BEST --
-connectDB();
-
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection failed:", err);
+  });
 
 // FIRST APPROACH -- COMMON --
 /*
