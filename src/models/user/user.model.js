0aa6cf2dcb3f521448ef7079ca6feb4b43726c46
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-12 16:47:46 +0500

    add user and video model

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -50,14 +50,40 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hashing the password before saving it to the database
-userSchema.pre("save", function(next){
-  if(!this.isModified("password")) return next(); // agr modify nhi howi password field 
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next(); // agr modify nhi howi password field
   this.password = bcrypt.hash(this.password, 10);
   next();
-})
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
-}
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
