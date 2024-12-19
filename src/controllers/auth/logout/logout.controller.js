dc732d68798009ee7668669ab81ca4f2b34ec1c8
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-19 19:24:02 +0500

    add logout controller half work

---
+++ src/controllers/auth/logout/logout.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../../../utils/asyncHandler.js";
+import { ApiError } from "../../../utils/ApiError.js";
+import { User } from "../../../models/user/user.model.js";
+import { uploadOnCloudinary } from "../../../utils/cloudinary.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
+
+
+const logout = asyncHandler(async (req, res) => {
+    // 
+});
