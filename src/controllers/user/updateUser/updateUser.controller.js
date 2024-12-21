48307cfef50974f7c4d2a29588a950d93f7db630
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-21 19:21:10 +0500

    add update-user controller only files

--- src/controllers/user/updateUser/updateUser.controller.js
+++ src/controllers/user/updateUser/updateUser.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js";
 import { ApiError } from "../../../utils/ApiError.js";
+import { uploadOnCloudinary } from "../../../utils/cloudinary.js";
 
 import { User } from "../../../models/user/user.model.js";
 
@@ -32,5 +33,73 @@ const updateUserDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "User updated successfully", user));
 });
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  // req.file -> avatar file
+  // find user by id
+  // update user avatar
+  // return response
 
-export { updateUserDetails };
+  const avatarLocalPath = req.file?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new ApiError(500, "Error while uploading on avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true,}
+  ).select("-password");
+
+  return res
+   .status(200)
+   .json(new ApiResponse(200, "Avatar updated successfully", user));
+
+});
+
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  // req.file -> coverImage file
+  // find user by id
+  // update user avatar
+  // return response
+
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image is required");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(500, "Error while uploading on coverImage");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true,}
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Cover image updated successfully", user));
+
+});
+
+export { updateUserDetails, updateUserAvatar, updateUserCoverImage };
