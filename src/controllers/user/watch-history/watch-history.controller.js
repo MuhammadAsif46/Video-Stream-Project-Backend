7ff76fb95a5ca0b88b71eafac08052fb9c704090
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-24 00:41:22 +0500

    add wactHistory endpoint

--- src/controllers/user/watch-history/watch-history.controller.js
+++ src/controllers/user/watch-history/watch-history.controller.js
@@ -1,35 +1,62 @@
-import {asyncHandler} from "../../../utils/asyncHandler.js"
-import {ApiError} from "../../../utils/ApiError.js"
-import {ApiResponse} from "../../../utils/ApiResponse.js"
-import {User} from "../../../models/user/user.model.js"
+import { asyncHandler } from "../../../utils/asyncHandler.js";
+import { ApiError } from "../../../utils/ApiError.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
+import { User } from "../../../models/user/user.model.js";
 import mongoose from "mongoose";
 
-const getUserWatchHistory = asyncHandler(async(req,res) => {
-    const user = await User.aggregate([
-        {
-            $match:{
-                _id: new mongoose.Types.ObjectId(req.user._id)
-            }
+const getUserWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localFields: "watchHistory",
+        foreignFields: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localFields: "owner",
+              foreignFields: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    // frontend person issue resolve direct pass object
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
         },
-        {
-            $lookup:{
-                from:"videos",
-                localFields:"watchHistory",
-                foreignFields:"_id",
-                as:"watchHistory",
-                pipeline:[
-                    {
-                        $lookup:{
-                            from:"users",
-                            localFields:"owner",
-                            foreignFields:"_id",
-                            as:"owner",
-                        }
-                    }
-                ]
-            }
-        }
-    ])
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "Watch History fetched successfully",
+        user[0].watchHistory
+      )
+    );
 });
 
-export {getUserWatchHistory}
+export { getUserWatchHistory };
