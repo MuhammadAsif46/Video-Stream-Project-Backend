6467d12ba88ad61c6230d1a8360762b3c8d86e07
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-25 13:03:19 +0500

    add like and comment models

---
+++ src/models/comment/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+// ability to control pagination from where to where:
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = model("Comment", commentSchema);
