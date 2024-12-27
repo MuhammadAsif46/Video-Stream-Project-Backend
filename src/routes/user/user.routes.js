6e026ca8579b2f38ed50229a121126ead4d21667
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-27 21:57:40 +0500

    add user routes updates

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
-import { registerUser } from "../../controllers/auth/registerUser/registerUser.controller.js";
-import { loginUser } from "../../controllers/auth/loginUser/loginUser.controller.js";
-import { refreshAccessToken } from "../../controllers/user/accessToken/accessToken.controller.js";
-import { logout } from "../../controllers/profile/logout/logout.controller.js";
-import { upload } from "../../middlewares/multer/multer.middleware.js";
-import { verifyJWT } from "../../middlewares/auth/auth.middleware.js";
-import { getCurrentUser } from "../../controllers/user/getUser/getUser.controller.js";
-import { changePasssword } from "../../controllers/profile/changePassword/changePassword.controller.js";
 import {
+  registerUser,
+  loginUser,
+  refreshAccessToken,
+  logout,
+  getCurrentUser,
+  changePassword,
   updateUserAvatar,
   updateUserDetails,
   updateUserCoverImage,
-} from "../../controllers/user/updateUser/updateUser.controller.js";
+  getUserWatchHistory,
+} from "../../controllers/user/index.js";
+import { upload } from "../../middlewares/multer/multer.middleware.js";
+import { verifyJWT } from "../../middlewares/auth/auth.middleware.js";
+
 import { getUserChannelProfile } from "../../controllers/channel-profile/channel-profile.controller.js";
-import { getUserWatchHistory } from "../../controllers/user/watch-history/watch-history.controller.js";
 
 const router = Router();
 
@@ -31,7 +32,7 @@ router.route("/login").post(loginUser);
 // Secure routes:
 router.route("/logout").post(verifyJWT, logout);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changePasssword);
+router.route("/change-password").post(verifyJWT, changePassword);
 router.route("/current-user").post(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateUserDetails);
 router
