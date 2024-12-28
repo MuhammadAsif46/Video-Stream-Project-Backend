998a056eecb6cde3825d58033556f89ad28d2964
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-28 23:33:20 +0500

    add video status and getVideoById controllers todos

---
+++ src/controllers/video/index.js
@@ -0,0 +1,3 @@
+import {deleteVideo} from "./delete-video/deleteVideo.controller.js"
+import {publishAVideo} from "./publish-video/publishVideo.controller.js"
+import {getVideoById} from "./get-VideoById/getVideoById.controller.js"
