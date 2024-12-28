9b255f82b5e1b7f1c12014e5b283e4caa3068dc4
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-28 23:40:01 +0500

    addd config video all controllers and routes

--- src/controllers/video/index.js
+++ src/controllers/video/index.js
@@ -1,3 +1,16 @@
-import {deleteVideo} from "./delete-video/deleteVideo.controller.js"
-import {publishAVideo} from "./publish-video/publishVideo.controller.js"
-import {getVideoById} from "./get-VideoById/getVideoById.controller.js"
+import { deleteVideo } from "./delete-video/deleteVideo.controller.js";
+import { publishAVideo } from "./publish-video/publishVideo.controller.js";
+import { getVideoById } from "./get-VideoById/getVideoById.controller.js";
+import { getAllVideos } from "./get-videos/getVideos.controller.js";
+import { getAllVideos } from "./get-videos/getVideos.controller.js";
+import { togglePublishStatus } from "./publish-video-status/publishVideoStatus.controller.js";
+import { updateVideo } from "./update-video/updateVideo.controller.js";
+
+export {
+  deleteVideo,
+  publishAVideo,
+  getAllVideos,
+  getVideoById,
+  togglePublishStatus,
+  updateVideo,
+};
