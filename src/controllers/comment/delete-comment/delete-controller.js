92c886436c52ff90be7f4251e15970dedad2dde3
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-29 16:29:51 +0500

    add comment todos with routes and controller

---
+++ src/controllers/comment/delete-comment/delete-controller.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+export const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+});
---
+++ src/controllers/comment/index.js
@@ -0,0 +1,6 @@
+import { addComment } from "./add-comment/add-comment.controller.js";
+import { updateComment } from "./update-comment/update-comment.controller.js";
+import { deleteComment } from "./delete-comment/delete-controller.js";
+import { getVideoComments } from "./get-comments/get-comments.controller.js";
+
+export { getVideoComments, addComment, updateComment, deleteComment };
